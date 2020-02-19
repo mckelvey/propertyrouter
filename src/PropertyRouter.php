6d28ba928272392be35b5bb506c2c1edd58af124
David McKelvey <david@mckelveycreative.co>
2020-02-18 17:00:48 -0800

    test pattern

--- src/PropertyRouter.php
+++ src/PropertyRouter.php
@@ -77,26 +77,30 @@ class PropertyRouter extends Plugin
         );
 
         Event::on(UrlManager::class, UrlManager::EVENT_REGISTER_SITE_URL_RULES, function(RegisterUrlRulesEvent $event) {
-            $types = array();
-            $property_types = Category::find()
-                ->group('propertyTypes')
-                ->all();
+            // $types = array();
+            // $property_types = Category::find()
+            //     ->group('propertyTypes')
+            //     ->all();
             // $type_criteria = craft()->elements->getCriteria(ElementType::Category)->group('propertyTypes');
-            foreach ((array) $property_types as $type) {
-              array_push($types, str_replace(' ', '-', $type->title));
-            }
-            $types = strtolower(implode('|', $types));
-            $term_pattern = '(for-sale|for-lease)+';
-            $types_pattern = '((' . $types . ')+(,(' . $types . ')+)*)+';
-            $search_pattern = '([-\w %"\*]){3,}';
+            // foreach ((array) $property_types as $type) {
+            //   array_push($types, str_replace(' ', '-', $type->title));
+            // }
+            // $types = strtolower(implode('|', $types));
+            // $term_pattern = '(for-sale|for-lease)+';
+            // $types_pattern = '((' . $types . ')+(,(' . $types . ')+)*)+';
+            // $search_pattern = '([-\w %"\*]){3,}';
 
-            $event->rules['properties/(?P<term>' . $term_pattern . ')/(?P<types>' . $types_pattern . ')/search:(?P<search>' . $search_pattern . ')'] = 'properties/index.html';
-            $event->rules['properties/(?P<term>' . $term_pattern . ')/search:(?P<search>' . $search_pattern . ')'] = 'properties/index.html';
-            $event->rules['properties/(?P<term>' . $term_pattern . ')/(?P<types>' . $types_pattern . ')'] = 'properties/index.html';
-            $event->rules['properties/(?P<types>' . $types_pattern . ')/search:(?P<search>' . $search_pattern . ')'] = 'properties/index.html';
-            $event->rules['properties/(?P<term>' . $term_pattern . ')'] = 'properties/index.html';
-            $event->rules['properties/(?P<types>' . $types_pattern . ')'] = 'properties/index.html';
-            $event->rules['properties/search:(?P<search>' . $search_pattern . ')'] = 'properties/index.html';
+            Craft::getLogger()->log('TEST LOG', 'info', $category = 'property-router')
+
+            $event->rules['properties/<term:(for-sale|for-lease)>'] = 'properties/index.html';
+
+            // $event->rules['properties/(?P<term>' . $term_pattern . ')/(?P<types>' . $types_pattern . ')/search:(?P<search>' . $search_pattern . ')'] = 'properties/index.html';
+            // $event->rules['properties/(?P<term>' . $term_pattern . ')/search:(?P<search>' . $search_pattern . ')'] = 'properties/index.html';
+            // $event->rules['properties/(?P<term>' . $term_pattern . ')/(?P<types>' . $types_pattern . ')'] = 'properties/index.html';
+            // $event->rules['properties/(?P<types>' . $types_pattern . ')/search:(?P<search>' . $search_pattern . ')'] = 'properties/index.html';
+            // $event->rules['properties/(?P<term>' . $term_pattern . ')'] = 'properties/index.html';
+            // $event->rules['properties/(?P<types>' . $types_pattern . ')'] = 'properties/index.html';
+            // $event->rules['properties/search:(?P<search>' . $search_pattern . ')'] = 'properties/index.html';
         });
     }
 
